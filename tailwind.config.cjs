378a0318a3d64ffca393b1a1cb652ab004b92fab
harappo <aravindmiras@gmail.com>
2023-04-03 11:15:32 +0530

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/*.astro','./src/content/*.astro','./src/layouts/*.astro','./src/pages/*.astro'],
+  content: ['./src/components/*.astro','./src/content/*.astro','./src/layouts/*.astro','./src/pages/*.astro','./src/pages/posts/*.astro',],
   theme: {
     extend: {},
   },
